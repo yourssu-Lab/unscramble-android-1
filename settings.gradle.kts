ca5feaa84c1b62f747c7c48658718a776b6c757c
kangyuri1114 <yuri3334@gmail.com>
2024-01-21 22:47:58 +0900

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "yourssu_rookies_unscramble"
+include(":app")
+ 
