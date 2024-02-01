26092c0ed6427985e04a8709362ab27233882dc7
yujin <jini1514@soongsil.ac.kr>
2024-02-02 02:18:48 +0900

    delete: 불필요한 임포트 제거

--- app/src/main/java/com/yourssu/unscramble/presentation/StartFragment.kt
+++ app/src/main/java/com/yourssu/unscramble/presentation/StartFragment.kt
@@ -3,17 +3,8 @@ package com.yourssu.unscramble.presentation
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.yourssu.design.system.atom.Picker
-import com.yourssu.design.system.foundation.Typo
-import com.yourssu.design.system.language.bottomSheet
-import com.yourssu.design.system.language.picker
-import com.yourssu.design.system.language.setLayout
-import com.yourssu.design.system.language.text
-import com.yourssu.design.undercarriage.size.dpToIntPx
 import com.yourssu.unscramble.R
 import com.yourssu.unscramble.databinding.FragmentStartBinding
 import com.yourssu.unscramble.util.base.BindFragment
