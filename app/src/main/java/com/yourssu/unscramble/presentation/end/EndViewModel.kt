3da2bdb54eb454a9ef5d29ba2950b912bbd79ebd
yujin <jini1514@soongsil.ac.kr>
2024-02-10 21:50:22 +0900

    refact: 불변값 StateFlow

--- app/src/main/java/com/yourssu/unscramble/presentation/end/EndViewModel.kt
+++ app/src/main/java/com/yourssu/unscramble/presentation/end/EndViewModel.kt
@@ -2,11 +2,12 @@ package com.yourssu.unscramble.presentation.end
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class EndViewModel : ViewModel() {
     private val _result: MutableStateFlow<String> = MutableStateFlow("Time out!")
-    val result: MutableStateFlow<String> = _result
+    val result: StateFlow<String> = _result
 
-    private val _score: MutableStateFlow<Int> = MutableStateFlow(100)
-    val score: MutableStateFlow<Int> = _score
+    private val _score: MutableStateFlow<Int> = MutableStateFlow(0)
+    val score: StateFlow<Int> = _score
 }
