6acb8331318f6bf119b1f85cc87bd869651ead41
yujin <jini1514@soongsil.ac.kr>
2024-02-11 01:34:58 +0900

    feat: 완료 여부가 playFragment에서 관찰이 안됨

--- app/src/main/java/com/yourssu/unscramble/presentation/play/PlayFragment.kt
+++ app/src/main/java/com/yourssu/unscramble/presentation/play/PlayFragment.kt
@@ -1,18 +1,24 @@
 package com.yourssu.unscramble.presentation.play
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.yourssu.unscramble.R
 import com.yourssu.unscramble.databinding.FragmentPlayBinding
+import com.yourssu.unscramble.presentation.timer.TimerViewModel
 import com.yourssu.unscramble.util.base.BindFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class PlayFragment : BindFragment<FragmentPlayBinding>() {
-    private val viewModel: PlayViewModel by viewModels()
+    private val playViewModel: PlayViewModel by viewModels()
+    private val timerViewModel: TimerViewModel by viewModels()
 
     override fun setBinding(layoutInflater: LayoutInflater): FragmentPlayBinding {
         return FragmentPlayBinding.inflate(layoutInflater)
@@ -21,12 +27,26 @@ class PlayFragment : BindFragment<FragmentPlayBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.viewModel = viewModel
+        binding.viewModel = playViewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
         binding.btnSubmit.setOnClickListener {
-            // Navigate to TimeFragment
             findNavController().navigate(R.id.endFragment)
+        }
+
+
+        Log.d("PlayFragment", timerViewModel.isEnd.value.toString())
+
+
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            timerViewModel.isEnd
+                .collectLatest { isEnd ->
+                    Log.d("PlayFragment", isEnd.toString())
+                    if (isEnd) {
+                        findNavController().navigate(R.id.endFragment)
+                    }
+                }
         }
     }
 }
