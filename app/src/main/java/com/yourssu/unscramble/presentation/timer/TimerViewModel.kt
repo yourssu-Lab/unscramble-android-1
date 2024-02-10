552a86464e8306c6ad77c2ea49afe832e4a92c85
yujin <jini1514@soongsil.ac.kr>
2024-02-11 00:58:09 +0900

    feat: 타이머 editText 뷰모델 주고받기 성공

--- app/src/main/java/com/yourssu/unscramble/presentation/timer/TimerViewModel.kt
+++ app/src/main/java/com/yourssu/unscramble/presentation/timer/TimerViewModel.kt
@@ -1,15 +1,25 @@
 package com.yourssu.unscramble.presentation.timer
 
+import android.text.Editable
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class TimerViewModel : ViewModel() {
-    private val _timerHour: MutableStateFlow<Int> = MutableStateFlow(0)
-    val timerHour: MutableStateFlow<Int> = _timerHour
-
     private val _timerMinute: MutableStateFlow<Int> = MutableStateFlow(0)
-    val timerMinute: MutableStateFlow<Int> = _timerMinute
+    val timerMinute: StateFlow<Int> = _timerMinute
+
+    private val _timerSecond: MutableStateFlow<Int> = MutableStateFlow(0)
+    val timerSecond: StateFlow<Int> = _timerSecond
 
     private val _timerButtonText: MutableStateFlow<String> = MutableStateFlow("START")
-    val timerButtonText: MutableStateFlow<String> = _timerButtonText
+    val timerButtonText: StateFlow<String> = _timerButtonText
+
+    fun updateMinute(s: Editable) {
+        _timerMinute.value = s.toString().toInt()
+    }
+
+    fun updateSecond(s: Editable) {
+        _timerMinute.value = s.toString().toInt()
+    }
 }
