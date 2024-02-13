546bf88d75d4521212b1df05bdb417524e370bb8
kangyuri1114 <yuri3334@gmail.com>
2024-02-14 00:14:27 +0900

    [chore] endFragment Score String 변환 코드 추가

--- app/src/main/java/com/yourssu/unscramble/presentation/play/PlayViewModel.kt
+++ app/src/main/java/com/yourssu/unscramble/presentation/play/PlayViewModel.kt
@@ -2,26 +2,23 @@ package com.yourssu.unscramble.presentation.play
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class PlayViewModel : ViewModel() {
     private val _questionWord: MutableStateFlow<String> = MutableStateFlow("")
-    val questionWord: MutableStateFlow<String> = _questionWord
+    val questionWord: StateFlow<String> = _questionWord.asStateFlow()
 
     private val _currentScore: MutableStateFlow<String> = MutableStateFlow("0")
-    val currentScore: MutableStateFlow<String> = _currentScore
+    val currentScore: StateFlow<String> = _currentScore.asStateFlow()
 
     private val _solvedProblem: MutableStateFlow<String> = MutableStateFlow("0")
-    val solvedProblem: MutableStateFlow<String> = _solvedProblem
+    val solvedProblem: StateFlow<String> = _solvedProblem.asStateFlow()
 
     private val _timerHour: MutableStateFlow<String> = MutableStateFlow("00")
-    val timerHour: MutableStateFlow<String> = _timerHour
+    val timerHour: StateFlow<String> = _timerHour.asStateFlow()
 
     private val _timerMinute: MutableStateFlow<String> = MutableStateFlow("00")
-    val timerMinute: MutableStateFlow<String> = _timerMinute
+    val timerMinute: StateFlow<String> = _timerMinute.asStateFlow()
 
-    private val _submitButtonText: MutableStateFlow<String> = MutableStateFlow("SUBMIT")
-    val submitButtonText: MutableStateFlow<String> = _submitButtonText
-
-    private val _skipButtonText: MutableStateFlow<String> = MutableStateFlow("SKIP")
-    val skipButtonText: MutableStateFlow<String> = _skipButtonText
 }
